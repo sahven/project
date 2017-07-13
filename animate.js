1a23782347a2110d1de983ff8dfb30156ac4bdfe
Vishal Maurya <vishalmr96@gmail.com>
2017-07-14 01:14:44 +0530

    inserted animation for links

--- animate.js
+++ animate.js
@@ -1,20 +1,108 @@
 $(document).ready(function() {
 
-	$(".fig_caption").hide();
+	$("span[class^='fig_caption']").hide();
 
+	//for p1
 	$(".p1").mouseenter(function() {
-		$(".link").css({
-			"width" : "500px"
+		$(".link-1").css({
+			"width" : "15%"
 		});
-		$(".fig_caption").fadeIn(200);
+		$(".fig_caption1").fadeIn();
 
 	});
 
 	$(".p1").mouseleave(function() {
-		$(".link").css({
-			"width" : "2%"
+		$(".link-1").css({
+			"width" : "0"
 		});
-		$(".fig_caption").fadeOut(1);
+		$(".fig_caption1").hide();
+
+	});
+
+	//for p2
+	$(".p2").mouseenter(function() {
+		$(".link-2").css({
+			"width" : "15%"
+		});
+		$(".fig_caption2").fadeIn();
+
+	});
+
+	$(".p2").mouseleave(function() {
+		$(".link-2").css({
+			"width" : "0"
+		});
+		$(".fig_caption2").hide();
+
+	});
+
+//for p3
+	$(".p3").mouseenter(function() {
+		$(".link-3").css({
+			"width" : "15%"
+		});
+		$(".fig_caption3").fadeIn();
+
+	});
+
+	$(".p3").mouseleave(function() {
+		$(".link-3").css({
+			"width" : "0"
+		});
+		$(".fig_caption3").hide();
+
+	});
+
+//for p4
+	$(".p4").mouseenter(function() {
+		$(".link-4").css({
+			"width" : "15%"
+		});
+		$(".fig_caption4").fadeIn();
+
+	});
+
+	$(".p4").mouseleave(function() {
+		$(".link-4").css({
+			"width" : "0"
+		});
+		$(".fig_caption4").hide();
+
+	});
+
+
+//for p5
+	$(".p5").mouseenter(function() {
+		$(".link-5").css({
+			"width" : "15%"
+		});
+		$(".fig_caption5").fadeIn();
+
+	});
+
+	$(".p5").mouseleave(function() {
+		$(".link-5").css({
+			"width" : "0"
+		});
+		$(".fig_caption5").hide();
+
+	});
+
+
+//for p6
+	$(".p6").mouseenter(function() {
+		$(".link-6").css({
+			"width" : "15%"
+		});
+		$(".fig_caption6").fadeIn();
+
+	});
+
+	$(".p6").mouseleave(function() {
+		$(".link-6").css({
+			"width" : "0"
+		});
+		$(".fig_caption6").hide();
 
 	});
 
