7bc44ac178618e6d6c57b564852b16a17877dd7d
Vishal Maurya <vishalmr96@gmail.com>
2017-07-19 21:04:40 +0530

    inserted dynamic navbar

---
+++ navbar.js
@@ -0,0 +1,23 @@
+var initial_height_navbar = 40;
+
+$(window).scroll(function() {
+
+	var final_height = $(this).scrollTop();
+
+	/*if ( Math.abs( final_height-initial_height_navbar ) > 0 )
+		return false;*/
+
+	if(final_height-initial_height_navbar > 0) {
+		$("#navbar-header").css({
+			"top" : "-45px"
+		});
+	}
+
+	else
+		$("#navbar-header").css({
+			"top" : ""
+		});
+
+	initial_height_navbar = final_height;
+
+});
