0c5fd5afbe52e966f12dee29bd1f72c71ed58cc1
Vishal Maurya <vishalmr96@gmail.com>
2017-07-19 21:08:32 +0530

    added some specific width to it

--- navbar.js
+++ navbar.js
@@ -4,19 +4,21 @@ $(window).scroll(function() {
 
 	var final_height = $(this).scrollTop();
 
-	/*if ( Math.abs( final_height-initial_height_navbar ) > 0 )
-		return false;*/
+	if ( Math.abs( final_height-initial_height_navbar ) > 5 ) {
+		
 
-	if(final_height-initial_height_navbar > 0) {
-		$("#navbar-header").css({
-			"top" : "-45px"
-		});
+		if(final_height-initial_height_navbar > 0) {
+			$("#navbar-header").css({
+				"top" : "-45px"
+			});
+		}
+
+		else
+			$("#navbar-header").css({
+				"top" : ""
+			});
+
 	}
-
-	else
-		$("#navbar-header").css({
-			"top" : ""
-		});
 
 	initial_height_navbar = final_height;
 
