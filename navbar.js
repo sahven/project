139497ea87b078f3096d645ed12ecaa36acee1c1
sahaj <sahaj.harekrishna@gmail.com>
2017-07-19 19:40:09 +0530

    added the fade in and out to the navbar

---
+++ navbar.js
@@ -0,0 +1,31 @@
+var e = '<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="http://www.iiti.ac.in/"><img src="images/gallery/logo.jpeg" width="37%" height="35px"></a>
+    </div>
+    <div class="collapse navbar-collapse">  
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+/*      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Pagename
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>   */
+        <li><a href="#">Gymkhana Office</a></li>
+        <li><a href="#">Hostels</a></li>
+        <li><a href="#">Tech CLubs</a></li>
+        <li><a href="#">Cultural Clubs</a></li>
+        <li><a href="#">Achievements and Photo Gallery</a></li>
+    </ul>
+    </div>    
+  </div>
+</nav>';
